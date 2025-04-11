02ca8f8d341837697d083127e2d295bef96911da
Frobama <benjamin.rojas.espejo@gmail.com>
2025-04-10 22:01:51 -0400

    barra de vida y spawner

--- player.gd
+++ player.gd
@@ -1,5 +1,8 @@
 extends CharacterBody2D
 
+signal health_depleted
+
+var health = 100.0
 
 func _physics_process(delta):
 	var direction = Input.get_vector("move_left","move_right","move_up","move_down")
@@ -12,7 +15,16 @@ func _physics_process(delta):
 		get_node("HappyBoo").play_walk_animation()
 	else:
 		get_node("HappyBoo").play_idle_animation()
-
+		
+	const DAMAGE_RATE = 5.0
+	var overlapping_mobs = %HurtBox.get_overlapping_bodies()
+	if overlapping_mobs.size() > 0:
+		health -= DAMAGE_RATE * overlapping_mobs.size() * delta
+		%ProgressBar.value = health
+		if health <= 0.0:
+			health_depleted.emit()
+			
+			
 func look_at_mouse():
 	var mouse_pos = get_global_mouse_position()
 	get_node("Gun/WeaponPivot").look_at(mouse_pos)
