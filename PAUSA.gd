d4e9980b97246969c930383f6eda53d0dda58e52
frank21-java <francisco.tegnologico@gmail.com>
2025-05-02 20:30:15 -0400

    volumen y pause

---
+++ PAUSA.gd
@@ -0,0 +1,16 @@
+extends CanvasLayer
+func _physics_process(delta: float):
+	if Input.is_action_just_pressed("pause"):
+		get_tree().paused = not get_tree().paused
+		$ColorRect.visible = not $ColorRect.visible
+		$Label.visible = not $Label.visible
+		$Salir.visible = not $Salir.visible
+
+func _on_salir_pressed():
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://menu.tscn")
+
+
+func _on_opcion_pressed() -> void:
+	get_tree().paused = false
+	get_tree().change_scene_to_file("res://opcion.tscn")
--- menu.gd
+++ menu.gd
@@ -1,14 +1,15 @@
 extends Button
+var sonidoFondo = preload("res://songs/ObservingTheStar.ogg")
 
-
+func _ready():
+	BocinaPrincipal.stream = sonidoFondo
+	BocinaPrincipal.play()
 func _on_pressed() -> void:
 	get_tree().change_scene_to_file("res://svgame.tscn")
 
 
 func _on_op_pressed() -> void:
 	get_tree().change_scene_to_file("res://opcion.tscn")
-
-
 
 func _on_exit_pressed() -> void:
 	get_tree().quit()
